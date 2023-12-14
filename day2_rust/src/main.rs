03fd296470cc16f302440f00509cfd429f3e87e0
Will Shivers <tayloe93@gmail.com>
2023-12-13 21:17:41 -0600

    Add part 2 for day 2

--- day2_rust/src/main.rs
+++ day2_rust/src/main.rs
@@ -32,21 +32,38 @@ impl Cubes {
             _ => panic!("Invalid property value {}", color),
         }
     }
+
+    pub fn get_color(&self, color: &str) -> i32 {
+      match color {
+        "red" => self.red,
+        "blue" => self.blue,
+        "green" => self.green,
+        _ => panic!("Invalid property value {}", color),
+      }
+    }
+
+    pub fn get_power(&self) -> i32 {
+      return self.red * self.blue * self.green;
+    }
 }
 
 fn main() {
     println!("Evalutating games...");
     let mut sum: i32 = 0;
+    let mut pwr_sum: i32 = 0;
     let mut path: std::path::PathBuf = env::current_dir().unwrap();
     path.push("src/input.txt");
 
     for line in read_to_string(path).unwrap().lines() {
         let valid_game_num: i32 = analyze_game(&line);
+        let min_game_pwr: i32 = get_power_of_min(&line);
 
         sum += valid_game_num;
+        pwr_sum += min_game_pwr;
     }
 
     println!("Sum of valid games: {}", sum);
+    println!("Sum of the power of the minimum sets: {}", pwr_sum);
 }
 
 fn analyze_game(inp: &str) -> i32 {
@@ -119,6 +136,43 @@ fn set_is_valid(game_dice: Cubes) -> bool {
     return true;
 }
 
+fn get_power_of_min(inp: &str) -> i32 {
+  let sets: Option<&str> = inp.split(":").last();
+  let min_cube: Cubes;
+
+  match sets {
+    Some(d) => min_cube = find_min_dice(d.trim()),
+    None => panic!("Could not find dice for game"),
+  }
+
+  return min_cube.get_power();
+}
+
+fn find_min_dice(game: &str) -> Cubes {
+  let mut min_cube: Cubes = Cubes {
+    red: 0,
+    blue: 0,
+    green: 0,
+  };
+
+  for set in game.split(";") {
+    let set_colors: str::Split<'_, &str> = set.split(",");
+
+    for dice in set_colors {
+      let mut dice_split: str::Split<'_, &str> = dice.trim().split(" ");
+      let dice_count: i32 = dice_split.next().unwrap().parse::<i32>().unwrap();
+      let dice_color: &str = dice_split.next().unwrap();
+      let current_color = min_cube.get_color(dice_color);
+
+      if current_color < dice_count {
+        min_cube = min_cube.set_color(dice_count, dice_color);
+      }
+    }
+  }
+
+  return min_cube;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -159,5 +213,21 @@ mod tests {
         };
 
         assert_eq!(false, set_is_valid(mock_cubes_invalid));
+    }
+
+    #[test]
+    fn find_min_dice_valid() {
+      let expected: Cubes = Cubes {
+        red: 2,
+        blue: 5,
+        green: 4
+      };
+
+      assert_eq!(expected, find_min_dice("3 blue, 4 green; 5 blue, 1 green, 2 red"));
+    }
+
+    #[test]
+    fn get_power_of_min_valid() {
+      assert_eq!(120, get_power_of_min("Game 5: 6 blue, 1 green; 2 blue, 4 red, 5 green"));
     }
 }
